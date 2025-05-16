8f37b9935b5e1ff7ad5b9a37aa4219ea1125d673
yu8-f <fukuyuki02817@icloud.com>
2025-05-16 12:27:29 +0900

    テストベンチを作成(rst時の挙動と，あと値が普通に若干ずれてる)

--- FPGA/FPGA_e2/build.ps1
+++ FPGA/FPGA_e2/build.ps1
@@ -13,5 +13,9 @@
 # vvp tb_e_calc.out
 
 # for top_e_display.v
-iverilog -o top_e_display.out init_400bit.v adder_400bit.v divider_400bit.v e_calc.v convert_to_10.v seg_hex.v top_e_display.v
-vvp top_e_display.out
+# iverilog -o top_e_display.out init_400bit.v adder_400bit.v divider_400bit.v e_calc.v convert_to_10.v seg_hex.v top_e_display.v
+# vvp top_e_display.out
+
+# for tb_top_e_display.v
+iverilog -o tb_top_e_display.out init_400bit.v adder_400bit.v divider_400bit.v e_calc.v convert_to_10.v seg_hex.v top_e_display.v tb_top_e_display.v
+vvp tb_top_e_display.out
